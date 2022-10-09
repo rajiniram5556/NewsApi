28bce84095c87459cee70feb6092f65c767f6032
Subbarami <subbarami@Nags-MacBook-Air.local>
2022-10-09 22:25:51 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopheadlinesComponent } from './topheadlines/topheadlines.component';
+import {HttpClientModule} from '@angular/common/http';
+import { TechComponent } from './tech/tech.component';
+import { SportsComponent } from './sports/sports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopheadlinesComponent,
+    TechComponent,
+    SportsComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
